f974cd5c3464d0665e35167c34408527f759bddc
Brian Fannin <FanninQED@Yahoo.com>
2016-12-30 11:11:04 -0500

    Initial commit

---
+++ r_min.py
@@ -0,0 +1,38 @@
+import sys
+
+from django.conf import settings
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisthesecretkey',
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse('This is my minimal R example')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
